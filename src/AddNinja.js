2fa36d5177af630cdb70e6a7dc1c1276d3d1b337
zan <zan_zibert@hotmail.com>
2018-10-31 17:30:13 +0100

    #17 - Forms Revisited

---
+++ src/AddNinja.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+class AddNinja extends Component{
+    state = {
+        name: null,
+        age: null,
+        belt: null
+    }
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
+    handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(this.state)
+    }
+    render(){
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="name">Name</label>
+                    <input onChange={this.handleChange} type="text" id="name"/>
+                    <label htmlFor="name">Age</label>
+                    <input onChange={this.handleChange} type="text" id="age"/>
+                    <label htmlFor="name">Belt</label>
+                    <input onChange={this.handleChange} type="text" id="belt"/>
+                    <button>Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default AddNinja
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Ninjas from './Ninjas'
+import AddNinja from './AddNinja'
 class App extends Component {
   state = {
     ninjas: [
@@ -14,6 +15,7 @@ class App extends Component {
         <h1>My first React App</h1>
         <p>Welcome!</p>
         <Ninjas ninjas={ this.state.ninjas } />
+        <AddNinja />
       </div>
     );
   }
