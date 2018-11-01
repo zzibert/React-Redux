febf811e47675f0724de7e63d16d2b9afd422d81
zan <zan_zibert@hotmail.com>
2018-11-01 17:29:31 +0100

    #40 - Mapping State to Props

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,5 +1,9 @@
 const initState = {
-    posts: []
+    posts: [
+        {id: '1', title: 'POST 1', body: 'this is the first post!'},
+        {id: '2', title: 'POST 2', body: 'this is the second post!'},
+        {id: '3', title: 'POST 3', body: 'this is the third post!'},
+    ]
 }
 
 const rootReducer = (state = initState, action) => {
